85d5769d10932068161c1ec8293cf9bfd0862dca
Rob Garrison <wowmotty@gmail.com>
2011-09-08 19:02:06 -0500

    initial commit

---
+++ README.markdown
@@ -0,0 +1,32 @@
+﻿These are some additional themes I put together for [AnythingSlider](http://proloser.github.com/AnythingSlider/)
+
+See them on the [AnythingSlider Theme Demo Page](http://mottie.github.com/AnythingSlider-Themes/)
+
+### Wrapper
+
+* The Wrapper images are not part of the AnythingSlider theme files and were included to provide some different backdrops in which to view the themes.
+* Please feel free to use the background images how ever you want - Where applicable, I used public domain images. 
+* All wrapper css is contained in the wrapper.css file.
+
+### Slider Images
+
+* All slider images are copyright Rob Macklin Photography and are used with his permission.
+* So, if you love the slider image(s) and want to get a copy, please don't steal it, contact him via his Flickr account: [riverboy777](http://www.flickr.com/photos/29247222@N08/)!
+
+### Theme notes
+
+* **Default (no images/css3)** - This theme was made as a base to build your own theme on. It doesn't use any images or css3.
+* **Default (with images/css3)** - Yes, this theme sucks... I didn't put a lot of effort into it but as I a second default theme, it was made to be used as a base model to build your own theme from :)
+* **Mini Dark** - The arrows are really hard to see, I know. I tried to add a glowy effect, but it just didn't look good to me. I'd recommend using this theme if you have a lot of light colored background images.
+* **Mini Light** - A little easier to see on dark background images ;)
+* **Office** - paperclips and thumb tacks... what else can I say.
+* **Polished** - I should have named this brushed aluminum, but the name was just too long and I like being lazy :P
+* **Ribbon** - Yeah, I know it's a metallic ribbon... if I was better at photoshop, I'd make it more silky, oh well.
+* **Shiny** - I like shiny stuff :P
+* **Tabs Light** - I really like this theme, but doesn't work too well when the slider's `navigationSize` option is set... the tabs don't fit inside of the navigation block because of the absolutely positioned span it needs. I'll need to modify the Slider code itself to account for this - when I get a chance.
+* **Tabs Dark** - This theme has the same problem when setting the `navigationSize` option.
+
+### Contribute
+
+* If you would like to fix a theme or contribute a new theme, then fork this repository, add your theme and send me a pull request.
+* Or you could just email me the files and I'll add it.
---
+++ demos/demo.js
@@ -0,0 +1,111 @@
+﻿$(function(){
+
+	// Set up Slider
+	$('#slider').anythingSlider({
+		theme : 'default1',
+//		navigationSize: 4,
+		navigationFormatter : function(i){
+			return [ 'Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5', 'Slide 6', 'Slide 7'][i-1];
+		}
+	})
+
+	// Add bottom caption FX
+	.anythingSliderFx({
+		'.caption-top' : [ 'caption-Top', '50px' ]
+	})
+
+	// Add a close button (x) to the caption
+	.find('div[class*=caption]')
+	.css({ position: 'absolute' })
+	.prepend('<span class="close">x</span>')
+	.find('.close').click(function(){
+		var cap = $(this).parent();
+		cap.animate({ top : -50 }, 400, function(){ cap.hide(); } );
+	});
+
+	// Theme Selector
+	var i, t, o = '', list1, list2,
+		ls = !!window.localStorage,
+		// all wrappers
+		wraps = [ 'None', 'Carbon Fiber', 'Glass', 'Glowy', 'Leather', 'Noise', 'Paper', 'Dark-wood', 'Light-wood' ],
+		// all themes
+		themes = [
+			// [ 'full name', 'id' ]
+			[ 'default1', 'Default (no images/css3)' ],
+			[ 'default2', 'Default (with images/css3)' ],
+			[ 'mini-dark', 'Mini Dark' ],
+			[ 'mini-light', 'Mini Light' ],
+			[ 'office', 'Office' ],
+			[ 'polished', 'Polished' ],
+			[ 'ribbon', 'Ribbon' ],
+			[ 'shiny', 'Shiny' ],
+			[ 'tabs-light', 'Tabs Light' ],
+			[ 'tabs-dark', 'Tabs Dark' ]
+		];
+
+	// Set up wrapper background selector
+	$.each(wraps, function(i){
+		o += '<option value="' + wraps[i].toLowerCase().replace(/\s/,'') + '">' + wraps[i] + '</option>';
+	});
+	list1 = $.map(wraps, function(t){ return t.toLowerCase().replace(/\s/,''); }).join(' ');
+	$('#wrap')
+	.html(o)
+	.change(function(){
+		$('#wrapper')
+		.removeClass(list1)
+		.addClass($(this).val());
+	});
+
+	// Set up theme selector
+	o = '';
+	$.each(themes, function(i){
+		o += '<option value="' + themes[i][0] + '">' + themes[i][1] + '</option>';
+	});
+	list2 = $.map(themes, function(t){ return 'anythingSlider-' + t[0]; }).join(' ');
+	$('#theme')
+	.html(o)
+	.change(function(){
+		var theme = $(this).val();
+		$('#slider').closest('div.anythingSlider')
+			.removeClass(list2)
+			.addClass('anythingSlider-' + theme);
+		$('#slider').anythingSlider(); // update slider - needed to fix navigation tabs
+	});
+
+	// Toggle slider state to view the differences in the css
+	$('button').click(function(){
+		var active = ($(this).text().match('Active')) ? 'addClass' : 'removeClass';
+		$('.anythingSlider')[active]('activeSlider');
+	});
+
+	// Dynamic background color - save background color and it's index to local storage
+	if (ls) {
+		t = localStorage.getItem("asbkgd") || '';
+		if (t) {
+			t = t.split(';');
+			$('body')[t[1] > 7 ? 'removeClass' : 'addClass']('dark').css({
+				backgroundColor : t[0]
+			});
+		}
+	}
+
+	// Change background color
+	o = '';
+	for (i=0; i < 15; i++){
+		t = i.toString(16);
+		o += '<span class="block" style="background-color:#' + t + t + t + '"></span>';
+	}
+	$('#bkgd')
+	.html(o)
+	.find('span').bind('mouseenter', function(){
+		var b = $(this).css('background-color'),
+		i = $(this).index();
+		$('body')[i > 7 ? 'removeClass' : 'addClass']('dark').css({
+				backgroundColor : b
+		});
+		if (ls) {
+			localStorage.setItem("asbkgd", [ b, i ].join(';'));
+		}
+	});
+
+});
